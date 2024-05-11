a25596d3f57274387394fcd27c7e53f911a03376
Charles Larroca <larroca_1000@hotmail.com>
2024-05-10 23:26:08 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // write your JavaScript here
+const btnMenuCheck = document.querySelector('#btn-menu-mobile')
+const btnMenu = document.querySelector('.btn-menu-mobile')
+
+btnMenu.addEventListener('click', function(){
+  if(btnMenuCheck.checked){
+    document.querySelector('.links-container-mobile').style.left = '-100%'
+    document.querySelector('.navbar-mobile').style.backgroundColor = 'transparent'
+    
+  } else {
+    document.querySelector('.links-container-mobile').style.left = '0'
+    document.querySelector('.navbar-mobile').style.backgroundColor = '#1E5AFA'
+  }
+})
